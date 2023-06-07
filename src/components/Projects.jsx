25c0db7d9ed88fab47f5de847f82ea1854bc32e1
“Pradaap-SS” <pradaapss@gmail.com>
2023-06-06 21:17:44 -0500

    message edits

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -93,7 +93,7 @@ const Projects = () => {
         >
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
+          links to code repositories and its deployed url. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
         </motion.p>
